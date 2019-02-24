e55ec8b0a1a54226040a46e5118b997fcf4b7660
GMJoker1964 <38500531+GMJoker1964@users.noreply.github.com>
2019-02-24 21:53:45 +0700

    Add files via upload

---
+++ HelloSb/src/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scan.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
